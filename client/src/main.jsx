6375e5b6570b0e036aac58ceb1cce8a789900e0c
Rostislav Sikora <Rostyslav Sikora>
2024-10-15 16:41:56 +0100

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from "./App.jsx";
+import { TransactionsProvider } from "./context/TransactionContext.jsx";
+import "./index.css";
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TransactionsProvider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <App />
+      </ClerkProvider>
+    </TransactionsProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
+
+
+
+
+
+
+
+
+
+
